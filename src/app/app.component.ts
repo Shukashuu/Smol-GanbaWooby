d4ddf9a40dc2467cd63437e4a99ae3a6045a0719
julyan <julyan0402@gmail.com>
2018-04-04 00:54:43 +0800

    Initial code

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Smol Wooby';
+  audio: any;
+
+  ngOnInit() {
+    this.audio = new Audio();
+    this.audio.src = '../assets/がんばるびぃ.mp3';
+    this.audio.load();
+    this.audio.loop = false;
+  }
+
+  loop() {
+    this.audio.loop = !this.audio.loop;
+  }
+
+  playAudio() {
+    this.audio.play();
+  }
+}
